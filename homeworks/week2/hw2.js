8738bcbc587eeadf4435b21acbf70c486de89b51
張皓銓 <>
2019-04-28 22:10:09 +0800

    hw2hw3 finished

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,13 @@
+let index;
+let answer;
+
 function capitalize(str) {
-  return str;
+  index = str.charCodeAt(0);
+
+  if (index >= 97 && index <= 122) {
+    answer = str.replace(String.fromCharCode(index), String.fromCharCode(index - 32));
+  }
+  return answer;
 }
 
 console.log(capitalize('hello'));
--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,13 @@
+const array = [];
+let i;
+let answer;
+
 function reverse(str) {
-  return str;
+  for (i = str.length; i >= 0; i -= 1) {
+    array.push(str.charAt(i));
+  }
+  answer = array.toString();
+  return console.log(answer);
 }
 
 reverse('hello');
