e6b9027449a3957a4ec41368aefb8e7518adf537
張皓銓 <>
2019-05-03 19:04:46 +0800

    test all homework files

--- homeworks/week3/hw2.test.js
+++ homeworks/week3/hw2.test.js
@@ -4,4 +4,13 @@ describe('hw2', () => {
   it('should return correct answer when str = nick', () => {
     expect(alphaSwap('nick')).toBe('NICK');
   });
+  it('should return correct answer when str = nick', () => {
+    expect(alphaSwap('david')).toBe('DAVID');
+  });
+  it('should return correct answer when str = nick', () => {
+    expect(alphaSwap('UpLoad')).toBe('uPlOAD');
+  });
+  it('should return correct answer when str = nick', () => {
+    expect(alphaSwap('123YoYa')).toBe('123yOyA');
+  });
 });
--- homeworks/week3/hw4.test.js
+++ homeworks/week3/hw4.test.js
@@ -4,4 +4,13 @@ describe('hw4', () => {
   it('should return correct answer when str = abcdcba', () => {
     expect(isPalindromes('abcdcba')).toBe(true);
   });
+  it('should return correct answer when str = abcdcba', () => {
+    expect(isPalindromes('123321')).toBe(true);
+  });
+  it('should return correct answer when str = abcdcba', () => {
+    expect(isPalindromes('abadcba')).toBe(false);
+  });
+  it('should return correct answer when str = abcdcba', () => {
+    expect(isPalindromes(' ')).toBe(true);
+  });
 });
