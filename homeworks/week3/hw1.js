4d114c2019b35dc1efeedfdc50812ae62f32d3d1
張皓銓 <>
2019-05-03 11:26:04 +0800

    hw1 finished

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,15 @@
 function stars(n) {
-  console.log(n);
+  const ansArray = [];
+  let starCount = '';
+
+  for (let i = 1; i <= n; i += 1) {
+    for (let j = 1; j <= i; j += 1) {
+      starCount += '*';
+    }
+    ansArray.push(starCount);
+    starCount = '';
+  }
+  return ansArray;
 }
 
 module.exports = stars;
--- homeworks/week3/hw1.test.js
+++ homeworks/week3/hw1.test.js
@@ -4,4 +4,10 @@ describe('hw1', () => {
   it('should return correct answer when n = 1', () => {
     expect(stars(1)).toEqual(['*']);
   });
+  it('should return correct answer when n = 1', () => {
+    expect(stars(3)).toEqual(['*', '**', '***']);
+  });
+  it('should return correct answer when n = 1', () => {
+    expect(stars(5)).toEqual(['*', '**', '***', '****', '*****']);
+  });
 });
