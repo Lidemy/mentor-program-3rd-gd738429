c009b0a7595e211a7ab53ca7cc4963e1c9ee2e26
張皓銓 <>
2019-05-03 14:26:38 +0800

    hw2 finished

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,15 @@
 function alphaSwap(str) {
-  console.log(str);
+  let ans = '';
+  for (let i = 0; i < str.length; i += 1) {
+    if (str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      ans += String.fromCharCode(str.charCodeAt(i) + 32);
+    } else if (str.charCodeAt(i) >= 97 && str.charCodeAt(i) <= 122) {
+      ans += String.fromCharCode(str.charCodeAt(i) - 32);
+    } else {
+      ans += str[i];
+    }
+  }
+  return ans;
 }
 
 module.exports = alphaSwap;
